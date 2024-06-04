2eee7e79587b91da1380834d02c643f2c59bcc7f
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-06-04 01:27:19 -0500

    curso vue fetch api2

--- 01-bases-js-ts/src/bases/10-fetch-api.ts
+++ 01-bases-js-ts/src/bases/10-fetch-api.ts
@@ -5,9 +5,11 @@ const apiKey = '18m2ulEtMBRpTSfiQ7ZY69kSHqvC9SbW';
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
     //.then(resp => console.log(resp))
-    .then(resp => resp.json())
+    .then((resp) => resp.json())
     /* .then(resp => {
         return resp.json()
     }) */
-    .then(body => console.log(body))
+    //.then((body) => console.log(body))
+    .then(({data}) => console.log({data}))
+    //no hay ninguntipo de data particular typescriopt no tien ecomo saber que data viene
     .catch(err => console.info(err));
