53472013fcfc819497dfc26e3877ca39b60888bf
oscar.giraldo <oscar.giraldo@linktic>
2024-06-06 16:48:15 -0500

    estilos externos tailwind

--- indesicion-app/src/components/my-counter-script2/MyCounterScript2.vue
+++ indesicion-app/src/components/my-counter-script2/MyCounterScript2.vue
@@ -6,7 +6,7 @@
         <h3>Square my counterscript logica separada: {{ squareCounter }}</h3>
 
         <div>
-            <button @click="counter++"> + 1 </button>
+            <button class="btn" @click="counter++"> + 1 </button>
             <button @click="counter--"> - 1 </button>
         </div>
     </section>
@@ -20,5 +20,9 @@
 <style scoped>
     h3{
         color: green;
+    }
+
+    .btn{
+        @apply p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2;
     }
 </style>
