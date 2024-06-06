32f8736c943841e16e27421dbadf1ce486af355f
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-06-05 21:12:36 -0500

    eventos

--- 02-intro-vue/ciclos/app.js
+++ 02-intro-vue/ciclos/app.js
@@ -1,6 +1,6 @@
 /* Frasaes de batman
  */
-const quotes = [
+const originalQuotes = [
     { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
     { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
     { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
@@ -30,9 +30,25 @@ const app = createApp({
         //const showAuthor = true;
         const showAuthor = ref(true);
 
+        //Variable reactiva
+        const quotes = ref(originalQuotes)
+
         //Primera forma
         const toggleAuthor = () => {
             showAuthor.value = !showAuthor.value 
+        }
+        
+        /**
+         * @description agregar un elemento al final de la list .push
+         * agregar un elemento al inicio de la lista .unshift
+         */
+        const aaddQuote = () => {
+            //quotes.unshift('Hola mundo')
+            /* Como estamos en js esto es permitido 
+            pero no deberia de serlo por que los quotes tienen una estructura
+            ya que tiene la frase y el author, por lo que es mejor hacer esta 
+            desestructuracioon */
+            quotes.value.unshift({quote: 'hola Mundo', author: 'Oscar Giraldo'})
         }
 
 
@@ -40,6 +56,7 @@ const app = createApp({
             quotes,
             showAuthor,
             toggleAuthor,
+            aaddQuote,
         }
     }
 });
