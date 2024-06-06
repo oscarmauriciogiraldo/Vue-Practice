b408f5cd7c6e24922e41b3d992448e73ad317a4e
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-06-06 01:36:30 -0500

    primer componente fin

--- indesicion-app/src/App.vue
+++ indesicion-app/src/App.vue
@@ -1,44 +1,16 @@
-<!-- Este es eñ unico componente que permite 
-el nombre del componente con una sola palabra  
-sfc -->
 <template>
-    <!-- <h1>Hola Vue</h1> -->
-    <!-- Eventos y estados -->
-    <section>
-        <h3>counter: {{ counter }}</h3>
-        <h3>Square: {{ 10 * 10 }}</h3>
-        <h3>Square2: {{ counter * counter }}</h3>
-        <h3>Square3: {{ squareCounter }}</h3>
-
-        <div>
-            <button @click="counter++"> + 1 </button>
-            <button @click="counter--"> - 1 </button>
-        </div>
-    </section>
+    <h1>Mi primera app</h1>
+    <hr>
+    <MyCounter />
 </template>
-<!-- Si no se refrescan los cambios 
-entonces ctrl shif y reload  -->
 
-<!-- Forma corta para decir que se 
-    trabaja con composition api -->
 <script lang="ts" setup>
-    import { computed, ref } from 'vue';
+import MyCounter from './components/MyCounter.vue';
+/* import { defineComponent } from 'vue' */
 
-    /* Basado en el counter puedo saber cual es el valor cuadrado */
-    //se define una variable reactiva
-    const counter = ref(2)
-
-    //Propiedad computada para calcular el squere
-    const squareCounter = computed(() => counter.value * counter.value)
-
-    /* console.log('hola mundo'); */
+/* export default defineComponent({
+    setup() {
+        
+    },
+}) */
 </script>
-
-<!-- optional el estilo -->
-<!-- scoped  es solo para aplicar estos estilos 
-    a este componente  -->
-<style scoped>
-    h1{
-        color: green;
-    }
-</style>
